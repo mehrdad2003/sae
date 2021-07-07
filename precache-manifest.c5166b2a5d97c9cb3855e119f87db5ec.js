ab7b78e2fbc68c5c9db0f491daa2e25389db76b1
unknown <mehrdadzare2003@gmail.com>
2021-07-07 08:24:47 +0430

    Updates

---
+++ precache-manifest.c5166b2a5d97c9cb3855e119f87db5ec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29338fe53725435fe9068f2c75ca189a",
+    "url": "/sae/index.html"
+  },
+  {
+    "revision": "47700be6d3ab8ef8b264",
+    "url": "/sae/static/css/main.f255b7dc.chunk.css"
+  },
+  {
+    "revision": "7605906012b84fbddce1",
+    "url": "/sae/static/js/2.51774bee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sae/static/js/2.51774bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47700be6d3ab8ef8b264",
+    "url": "/sae/static/js/main.cbb3048e.chunk.js"
+  },
+  {
+    "revision": "9a364779bba1409bdbc3",
+    "url": "/sae/static/js/runtime-main.662e7900.js"
+  },
+  {
+    "revision": "032b81581f0320648ca042fb13bf8751",
+    "url": "/sae/static/media/01.032b8158.jpg"
+  },
+  {
+    "revision": "bab5e043e4fc4bcce51699f943003c85",
+    "url": "/sae/static/media/IRANSansWeb.bab5e043.ttf"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/sae/static/media/svg-1.715313cd.svg"
+  },
+  {
+    "revision": "ea85e7aa8379f1549481b5fa40cc3c6a",
+    "url": "/sae/static/media/svg-2.ea85e7aa.svg"
+  },
+  {
+    "revision": "b953e5b996d894979a122f36ea4ea945",
+    "url": "/sae/static/media/svg-3.b953e5b9.svg"
+  }
+]);
